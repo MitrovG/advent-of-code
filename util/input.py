6009dbb83717be4cfa8203f26660b0a0dc4b4af9
Goran Mitrov <goranmitrov996@gmail.com>
2022-11-30 22:36:13 +0100

    Initial input module

---
+++ util/input.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+def read_integer_list(filepath: str) -> List[int]:
+    with open(filepath, 'r') as fin:
+        integers = [int(num) for num in fin.read().split('\n')]
+
+    return integers
+
+
+def read_string_list(filepath: str) -> List[str]:
+    with open(filepath, 'r') as fin:
+        data = fin.read().split('\n')
+
+    return data
