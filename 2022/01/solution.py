470387bd80d0594ed5043cbb1907775043df05fe
Goran Mitrov <goranmitrov@rocketmail.com>
2022-12-01 08:49:55 +0100

    2022 day 1 solution

--- 2022/01/solution.py
+++ 2022/01/solution.py
@@ -5,3 +5,20 @@ from util.input import read_integer_list, read_string_list
 directory_name = os.path.dirname(os.path.abspath(__file__))
 filepath = os.path.join(directory_name, 'input.txt')
 
+data = read_string_list(filepath)
+
+calories = []
+elf_calories = 0
+for row_calorie in data:
+    if not row_calorie:
+        calories.append(elf_calories)
+        elf_calories = 0
+    else:
+        elf_calories += int(row_calorie)
+calories.sort(reverse=True)
+
+solution_1 = calories[0]
+solution_2 = sum(calories[:3])
+
+print(solution_1)
+print(solution_2)
