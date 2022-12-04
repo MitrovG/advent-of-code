a0a0a39811c3a11b05c18cfe8804a8a881c78880
Goran Mitrov <goranmitrov@rocketmail.com>
2022-12-04 09:34:22 +0100

    2022 day 4 solution

--- 2022/04/solution.py
+++ 2022/04/solution.py
@@ -11,8 +11,8 @@ solution_1 = 0
 solution_2 = 0
 for pair_of_elves in data:
     elf_1, elf_2 = pair_of_elves.split(',')
-    elf_1_min, _, elf_1_max = map(lambda x: int(x) if x != '-' else -1, elf_1.partition('-'))
-    elf_2_min, _, elf_2_max = map(lambda x: int(x) if x != '-' else -1, elf_2.partition('-'))
+    elf_1_min, elf_1_max = map(int, elf_1.split('-'))
+    elf_2_min, elf_2_max = map(int, elf_2.split('-'))
     if (elf_1_min >= elf_2_min and elf_1_max <= elf_2_max) or \
         (elf_2_min >= elf_1_min and elf_2_max <= elf_1_max):
         solution_1 += 1
